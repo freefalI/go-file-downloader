53850aedfc4c7a7009dfcdecb04614d6433b2f78
Andrew Shmatko <andriyshmatko1999@gmail.com>
2024-09-21 01:57:11 +0300

    Add logic of downloading file

--- cmd/main.go
+++ cmd/main.go
@@ -4,9 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/olekukonko/tablewriter"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"path"
+	"path/filepath"
 	"sync"
 	"time"
 )
@@ -18,15 +21,19 @@ type File struct {
 	StartedAt  time.Time `json:"started_at"`
 	FinishedAt time.Time `json:"finished_at"`
 	Progress   int       `json:"progress"`
+	Size       int       `json:"size"`
+	IsDone     bool      `json:"is_done"`
 }
 
 var (
 	mu    sync.Mutex
-	files = []File{
-		{1, "http://", "nam", time.Now(), time.Now().Add(time.Minute), 20},
+	files = []*File{
+		// {1, "http://", "nam", time.Now(), time.Now().Add(time.Minute), 20},
 	}
-	nextID = 2 // To generate unique IDs for each file
+	nextID = 1 // To generate unique IDs for each file
 )
+
+const downloadDir = "Downloads"
 
 // Handler to get all files
 func getFiles(w http.ResponseWriter, r *http.Request) {
@@ -57,7 +64,7 @@ func addFile(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
 
 	// Create a new File entry
-	file := File{
+	file := &File{
 		ID:        nextID,
 		URL:       newFile.URL,
 		Name:      "",
@@ -73,6 +80,11 @@ func addFile(w http.ResponseWriter, r *http.Request) {
 
 	mu.Unlock()
 
+	log.Println("New file added")
+
+	// Process the job immediately in a goroutine
+	go downloadFile(file)
+
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(file)
 }
@@ -82,6 +94,8 @@ func main() {
 	http.HandleFunc("/files/add", addFile)
 
 	log.Println("Server is running on port :8080")
+
+	createDownloadsDir()
 
 	ticker := time.NewTicker(200 * time.Millisecond)
 	defer ticker.Stop()
@@ -105,7 +119,7 @@ func main() {
 
 func setupTable() *tablewriter.Table {
 	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"#", "URL", "Name", "Started at", "Finished at", "Progress"})
+	table.SetHeader([]string{"#", "URL", "Name", "Started at", "Finished at", "Size", "Progress", "Done"})
 	table.SetBorder(true)
 
 	return table
@@ -115,13 +129,20 @@ func printEntries(table *tablewriter.Table) {
 	table.ClearRows()
 
 	for _, file := range files {
+		var finishedAt string
+		if !file.FinishedAt.IsZero() {
+			finishedAt = file.FinishedAt.Format(time.DateTime)
+		}
+
 		table.Append([]string{
 			fmt.Sprintf("%d", file.ID),
 			file.URL,
 			file.Name,
-			file.StartedAt.Format(time.RFC1123),
-			file.FinishedAt.Format(time.RFC1123),
+			file.StartedAt.Format(time.DateTime),
+			finishedAt,
+			fmt.Sprintf("%dMb", file.Size),
 			fmt.Sprintf("%d%%", file.Progress),
+			fmt.Sprintf("%t", file.IsDone),
 		})
 	}
 
@@ -131,4 +152,87 @@ func printEntries(table *tablewriter.Table) {
 
 func clearTerminal() {
 	os.Stdout.WriteString("\033[H\033[2J")
+}
+
+func createDownloadsDir() {
+	// Create a "Downloads" directory in the current working directory
+	err := os.MkdirAll(downloadDir, os.ModePerm)
+	if err != nil {
+		fmt.Printf("Error creating Downloads directory: %v\n", err)
+		return
+	}
+}
+
+func downloadFile(fileObject *File) {
+	log.Printf("File with id:%d is processing", fileObject.ID)
+
+	// Get the data
+	response, err := http.Get(fileObject.URL)
+	if err != nil {
+		log.Printf("failed to download file: %s", err)
+		return
+	}
+	defer response.Body.Close()
+
+	// Check if the response status is OK
+	if response.StatusCode != http.StatusOK {
+		log.Printf("failed to download file: %s", response.Status)
+		return
+	}
+
+	// Create the file
+	fileName := path.Base(response.Request.URL.Path)
+	fileObject.Name = fileName
+	filePath := filepath.Join(downloadDir, fileName)
+	file, err := os.Create(filePath)
+	if err != nil {
+		log.Printf("failed to create file: %s", err)
+		return
+	}
+	defer file.Close()
+
+	// Get the content length
+	totalBytes := response.ContentLength
+	fmt.Println(totalBytes)
+	var downloadedBytes int64
+
+	sizeKnown := totalBytes > 0
+	// Create a buffer to hold chunks of data
+	buffer := make([]byte, 32*1024) // 32 KB buffer
+
+	if sizeKnown {
+		fileObject.Size = int(totalBytes) / 1024 / 1024
+	}
+	// Download the file
+	for {
+		// Read a chunk from the response body
+		bytesRead, err := response.Body.Read(buffer)
+		if err != nil && err != io.EOF {
+			log.Printf("failed to read response: %s", err)
+			return
+		}
+		if bytesRead == 0 {
+			break // End of file
+		}
+
+		// Write the chunk to the file
+		_, err = file.Write(buffer[:bytesRead])
+		if err != nil {
+			log.Printf("failed to write to file: %s", err)
+			return
+		}
+
+		// Update the downloaded bytes count
+		downloadedBytes += int64(bytesRead)
+		if !sizeKnown {
+			fileObject.Size = int(downloadedBytes) / 1024 / 1024
+		}
+		if sizeKnown {
+			fileObject.Progress = int(float64(downloadedBytes) / float64(totalBytes) * 100)
+		}
+	}
+	fileObject.IsDone = true
+	fileObject.FinishedAt = time.Now()
+	
+	fmt.Printf("\nDownloaded file: %s\n", filePath)
 }
